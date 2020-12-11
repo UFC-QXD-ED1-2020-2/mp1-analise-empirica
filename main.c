8da66f2b1947d8c48bc7b2ff9ff62ee786ae530b
Arthur Araruna <ararunaufc@gmail.com>
2020-12-11 15:48:38 -0300

    Prepara projeto para benchmark
    - Usa biblioteca ubench.h

---
+++ main.c
@@ -0,0 +1,36 @@
+/**
+ **
+ **  @file   main.c
+ **  @date   2020-12-10
+ **  @author Arthur Rodrigues Araruna <ararunaufc(at)gmail.com>
+ **
+ **  @brief
+ **  Arquivo principal, cuja única responsabilidade consiste de iniciar
+ **  a biblioteca que realiza os benchmarks.
+ **
+ **  @copyright
+ **  Copyright (C) 2020 Universidade Federal do Ceará.
+ **
+ **  This source code is part of an educational project, associated with
+ **  an undergraduate course or extension project, that is hosted at
+ **  Universidade Federal do Ceará, Campus Quixadá.
+ **
+ **  This project is free software: you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation, either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  This project is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with the source code of this project. If not, see
+ **  <http://www.gnu.org/licenses/>.
+ **
+ **/
+
+#include <ubench.h>
+
+UBENCH_MAIN()
